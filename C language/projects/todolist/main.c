65452b1a311601cea9cda58c4dd35b5f5f5281b5
ced3j <spoonnoisthere@gmail.com>
2025-08-18 17:42:20 +0300

    todolist example second version

--- C language/projects/todolist/main.c
+++ C language/projects/todolist/main.c
@@ -1,50 +1,139 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
 
+#define MAX_GOREV 100
 
-struct Gorevler{
-	char gorevAdi[50];
-	char gorevIcerik[100];
+struct Gorevler {
+    char gorevAdi[50];
+    char gorevIcerik[100];
+    uint8_t tamamlandi;
 };
 
+void gorevleriGoster(struct Gorevler* gorevler, int length) {
+    if (length == 0) {
+        printf("\n--- Hic kayit yok! ---\n");
+        return;
+    }
 
-int menuScreen(){
-	int gorevSay;
-	printf("-------- TODOLIST ---------\n");
-	printf("Kac gorev kaydetmek istiyorsunuz?\n");
-	scanf("%d", &gorevSay);	
-	
-	return gorevSay;
+    for (int i = 0; i < length; i++) {
+        printf("\nGorev: %d\n", i + 1);
+        printf("Adi   : %s\n", gorevler[i].gorevAdi);
+        printf("Icerik: %s\n", gorevler[i].gorevIcerik);
+        printf("Durum : %s\n", gorevler[i].tamamlandi ? "Tamamlandi" : "Yapilacak");
+    }
+}
+
+void gorevEkle(struct Gorevler* gorevler, int* length) {
+    if (*length >= MAX_GOREV) {
+        printf("Maksimum kayit sayisina ulasildi!\n");
+        return;
+    }
+
+    struct Gorevler yeni;
+
+    getchar(); // scanf'ten kalan \n temizleniyor
+
+    printf("Gorev adi: ");
+    fgets(yeni.gorevAdi, sizeof(yeni.gorevAdi), stdin);
+
+    printf("Gorev icerigi: ");
+    fgets(yeni.gorevIcerik, sizeof(yeni.gorevIcerik), stdin);
+
+    printf("Tamamlandi mi? (0: hayir, 1: evet): ");
+    scanf("%hhu", &yeni.tamamlandi);
+
+    gorevler[*length] = yeni;
+    (*length)++;
+
+    printf("Yeni gorev eklendi!\n");
+}
+
+void gorevSil(struct Gorevler* gorevler, int* length) {
+    if (*length == 0) {
+        printf("Hic kayit yok, silemezsiniz.\n");
+        return;
+    }
+
+    int silinecek;
+    printf("Silmek istediginiz gorevin numarasini girin (1-%d): ", *length);
+    scanf("%d", &silinecek);
+
+    if (silinecek < 1 || silinecek > *length) {
+        printf("Gecersiz numara!\n");
+        return;
+    }
+
+    // Elemanlari kaydir
+    for (int i = silinecek - 1; i < *length - 1; i++) {
+        gorevler[i] = gorevler[i + 1];
+    }
+
+    (*length)--; // toplam kayit sayisini azalt
+    printf("Gorev silindi!\n");
+}
+
+int main() {
+    struct Gorevler gorevler[MAX_GOREV];
+    int length = 0; // kayit sayisi
+    int secim;
+
+    while (1) {
+        printf("\n--- MENU ---\n");
+        printf("1. Yeni kayit ekle\n");
+        printf("2. Kayitlari listele\n");
+        printf("3. Kaydi sil\n");
+        printf("0. Cikis\n");
+        printf("Seciminiz: ");
+        scanf("%d", &secim);
+
+        switch (secim) {
+            case 1:
+                gorevEkle(gorevler, &length);
+                break;
+            case 2:
+                gorevleriGoster(gorevler, length);
+                break;
+            case 3:
+                gorevSil(gorevler, &length);
+                break;
+            case 0:
+                printf("Programdan cikiliyor...\n");
+                return 0;
+            default:
+                printf("Gecersiz secim!\n");
+        }
+    }
+
+    return 0;
 }
 
 
-void gorevleriGoster(struct Gorevler* gorevler, int length){
+
+/*
+
+scanf("%hhu", &yeni.tamamlandi); 
 	
-	for(int i = 0; i < length; i++){
-		printf("Gorev: %d\n", i+1);
-		printf("Adi: %s\n", gorevler[i].gorevAdi);
-		printf("Icerik: %s\n\n\n", gorevler[i].gorevIcerik);
-	}
-}
+	Neden hhu?
+
+	tamamlandi degiskeninin tipi "uint8_t"  bu aslinda unsigned char tipine denk gelir
+	%d -> int
+	%u -> unsigned int
+	%hhu -> unsigned char (8bit)
+	 
 
 
-int main(){
-	int input = menuScreen();
-	struct Gorevler gorevler[input];
-	
-	
-	for(int i = 0; i < input; i++){
-		printf("Gorev %d adini girin: ", i+1);
-		scanf("%s\n", gorevler[i].gorevAdi);
-		
-		printf("Gorev %d icerigi girin: ", i+1);
-		scanf("%s\n", gorevler[i].gorevIcerik);
-	}
-	
-	
-	gorevleriGoster(gorevler, input);
-	
 
-	return 0;
+*/
 
-}
+
+
+
+
+
+
+
+
+
+
+
